149ef5c7f75a2a6be8de1c07d06fff1c0373c2be
seror <seror94ab@yahoo.com>
2018-05-24 18:27:48 -0500

    added friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,169 @@
-// var friends = [{
-//     Name: "FDVSFSFVFV"
-//     photo:"url"
-//     score:[1,3,4,1]
-// }]
+// Data structure that will store the friends data
+var friendsArray = [
+    {
+        name: "Swag",
+        photo: "https://www.gettyimages.co.uk/gi-resources/images/Embed/new/embed2.jpg",
+        scores: [
+            '1',
+            '5',
+            '5',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: "Mason",
+        photo: "https://r.hswstatic.com/w_907/gif/nonhuman-animals-jobs-1.jpg",
+        scores: [
+            '2',
+            '4',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '2',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: "Bob",
+        photo: "https://i.pinimg.com/736x/f3/4a/70/f34a706f09c995cb2bece1077dfa1874--funny-animal-videos-funny-videos.jpg",
+        scores: [
+            '4',
+            '3',
+            '5',
+            '1',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1'
+        ]
+    },
+    {
+        name: "Mr. Lazy",
+        photo: "http://www.vitamin-ha.com/wp-content/uploads/2012/06/VH-funny-animals-14.jpg",
+        scores: [
+            '5',
+            '1',
+            '2',
+            '1',
+            '3',
+            '4',
+            '1',
+            '3',
+            '3',
+            '4'
+        ]
+    },
+    {
+        name: "Hank",
+        photo: "https://static.boredpanda.com/blog/wp-content/uploads/2015/12/funny-animal-pictures-comedy-wildlife-photography-awards-28__880.jpg",
+        scores: [
+            '1',
+            '1',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '1'
+        ]
+    },
+    {
+        name: "John",
+        photo: "https://kids.nationalgeographic.com/content/dam/kids/photos/articles/Other%20Explore%20Photos/R-Z/Wacky%20Weekend/Funny%20Animal%20Faces/ww-funny-animal-faces-hippopotamus.adapt.945.1.jpg",
+        scores: [
+            '1',
+            '1',
+            '5',
+            '1',
+            '3',
+            '2',
+            '5',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: "Dan",
+        photo: "https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/All-about-puppies--Cesar%E2%80%99s-tips%2C-tricks-and-advice.jpg?itok=bi9xUvwe",
+        scores: [
+            '5',
+            '4',
+            '4',
+            '2',
+            '4',
+            '4',
+            '3',
+            '2',
+            '3',
+            '3'
+        ]
+    },
+    {
+        name: "Jeff",
+        photo: "https://i.ytimg.com/vi/rziIg5V1RdA/maxresdefault.jpg",
+        scores: [
+            '5',
+            '2',
+            '3',
+            '1',
+            '5',
+            '1',
+            '3',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: "Suzan",
+        photo: "https://www.stayathomemum.com.au/cache/860x380-0/wp-content/uploads/2015/10/Teacup-Pomeranian-White-e1446515202248.jpg",
+        scores: [
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5'
+        ]
+    },
+    {
+        name: "Martha",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRxKbwhrSYn9flZFe55w61F5voJzY8B97h2XhbzgzLGt55N1yu",
+        scores: [
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1'
+        ]
+    }
+];
 
 
 //export this array
+module.exports = friendsArray;
+
+

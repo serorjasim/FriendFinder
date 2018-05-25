72e2b749fb32de97d94bc6e38bc3204a9d44eb67
seror <seror94ab@yahoo.com>
2018-05-24 19:13:11 -0500

    apiRoutes update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,11 @@
 // require the friends.js file
 
+var require = require("app/data/friends");
+
+
+//module.exports fuction(app)
+
+
 // app.get the friends.js and write function to grab the array of friends
 
 // app.post create for loop and an if else statement

321b8858764c6ad03fba2b797408335e91cda1a4
seror <seror94ab@yahoo.com>
2018-05-24 23:33:12 -0500

    final

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,45 @@
 // require the friends.js file
 
-var require = require("app/data/friends");
+var friends = require("../data/friends.js");
 
 
 //module.exports fuction(app)
 
+module.exports = function (app) {
 
-// app.get the friends.js and write function to grab the array of friends
 
-// app.post create for loop and an if else statement
+    // app.get the friends.js and write function to grab the array of friends
+
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
+
+    // app.post create for loop and an if else statement
+
+    app.post("/api/friends", function (req, res) {
+        var userInput = req.body;
+        var userResponses = userInput.scores;
+        var matchName = "";
+        var matchImage = "";
+        var totalDifference = 4000;
+
+        for (var i = 0; i < friends.length; i++) {
+
+            var difference = 0;
+
+            for (var j = 0; j < userResponses.length; j++) {
+                difference += Math.abs(friends[i].scores[j] - userResponses[j]);
+            }
+
+            if (difference < totalDifference) {
+                totalDifference = difference;
+                matchName = friends[i].name;
+                matchImage = friends[i].photo;
+            }
+        }
+        friends.push(userInput);
+        res.json({ status: "OK", matchName: matchName, matchImage: matchImage });
+    });
+
+};
+

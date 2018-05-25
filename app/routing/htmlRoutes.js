55b5162437504182cfc825b5f7397c8adce32027
seror <seror94ab@yahoo.com>
2018-05-24 19:08:03 -0500

    adding html routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,5 +1,18 @@
 //require the path
 
-//module.exports fuction(app)
+var path = require("path");
 
+//module.exports fuction(app)
 //link to html pages using the routes
+
+module.exports = function (app) {
+
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "app/public/survey.html"))
+    });
+
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "app/public/home.html"))
+    });
+};
+
